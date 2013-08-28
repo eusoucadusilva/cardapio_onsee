4294e13835e0df073f18d18d66486b3e9ef0a72e
carlos silva <cefs1984@gmail.com>
2013-08-27 22:41:37 -0300

    commit inicial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardapioOnsee::Application.config.secret_token = 'b80620c9ddd0ff29a52a5a374d49355041e25d6c6a6d2d7376a03dab1df385001ac5f7db4d78e60844e2778f61f79ae6010cf5d041bd44fc5d868fcb5468843f'
