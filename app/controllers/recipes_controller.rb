cff43eec0af393f359d4c2e06717f06ce8810efd
carlos silva <cefs1984@gmail.com>
2013-08-29 15:26:28 -0300

    Mudar redirect_to do create e update.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -53,7 +53,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.save
-        format.html { redirect_to @recipe, notice: 'Receita criada com sucesso.' }
+        format.html { redirect_to edit_recipe_path(@recipe), notice: 'Receita criada com sucesso.' }
         format.json { render json: @recipe, status: :created, location: @recipe }
       else
         format.html { render action: "new" }
@@ -67,7 +67,7 @@ class RecipesController < ApplicationController
 
     respond_to do |format|
       if @recipe.update_attributes(params[:recipe])
-        format.html { redirect_to @recipe, notice: 'Receita atualizada com sucesso.' }
+        format.html { redirect_to edit_recipe_path(@recipe), notice: 'Receita atualizada com sucesso.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
