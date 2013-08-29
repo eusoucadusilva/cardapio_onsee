33e56c52471304fe302ccafdee96f4864c79f1c6
carlos silva <cefs1984@gmail.com>
2013-08-29 15:37:31 -0300

    Testado Criação de Receitas via Spec/Requests.

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,11 +1,36 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
-describe "Recipes" do
-  describe "GET /recipes" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get recipes_path
-      response.status.should be(200)
-    end
+feature "Recipes" do
+  let(:paulo_moura) { User.make!(:paulo_moura) }
+
+  background do
+    login_as paulo_moura, scope: :user
+
+    visit root_path
+  end
+
+  scenario 'create recipe' do
+    expect(page).to have_content 'Bem vindo, paulo@ons.ee'
+
+    click_link 'Bem vindo, paulo@ons.ee'
+    click_link 'Cadastrar Receita'
+
+    expect(page).to have_content 'Divulgue sua receita para o mundo!'
+
+    fill_in 'Título', with: 'X-Tudo Paraense'
+    fill_in 'Descrição', with: 'Leva Pão, Queijo, Açaí e Camarão.'
+
+    click_button 'Criar Receita'
+
+    expect(page).to have_content 'Receita criada com sucesso.'
+
+    expect(page).to have_content 'Atualize sua receita.'
+
+    expect(page).to have_field 'Título', 'X-Tudo Parense'
+    expect(page).to have_field 'Descrição', 'Leva Pão, Queijo, Açaí e Camarão.'
+
+    print page.html
   end
 end
