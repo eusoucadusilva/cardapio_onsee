05635d297d7578c9088f757057d88e09ae81af51
carlos silva <cefs1984@gmail.com>
2013-08-29 15:36:32 -0300

    Criando Blueprints de User e Recipe

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,9 +1,16 @@
+# encoding: utf-8
+
 require 'machinist/active_record'
 
-# Add your blueprints here.
-#
-# e.g.
-#   Post.blueprint do
-#     title { "Post #{sn}" }
-#     body  { "Lorem ipsum..." }
-#   end
+User.blueprint(:paulo_moura) do
+  name { 'Paulo Moura' }
+  email { 'paulo@ons.ee' }
+  password { '12345678' }
+  password_confirmation { '12345678' }
+end
+
+Recipe.blueprint(:acai_com_carne_de_sol) do
+  avatar { 'imagem1.png' }
+  title { 'Açaí com carne de sol' }
+  description { 'Meus ingredientes são açaí e carne de sol' }
+end
